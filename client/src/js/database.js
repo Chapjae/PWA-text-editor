33670e3383ed85130a08991efa080b774aeab720
Jay Liu <jayliu89@gmail.com>
2023-08-29 15:43:59 -0700

    removed console.logs in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,6 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-  console.log('result.value', result);
   return result.value;
 }
 
